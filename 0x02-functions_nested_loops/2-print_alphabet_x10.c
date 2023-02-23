7a8d0a364dab510e99cd0fa0ad220eeb8db24209
iAmEmmanuelist <emmanuelpaul152@gmail.com>
2023-02-23 01:23:36 +0100

    Modify return value

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
   * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
   * followed by a new line.
   *
-  * Return: On success 1.
+  * Return: always 0
   */
 
 void print_alphabet_x10(void)
