4b5e4439ef48bfaf86ad2a26c4b11addcfb22222
iAmEmmanuelist <emmanuelpaul152@gmail.com>
2023-02-24 16:40:03 +0100

    Function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _isdigit - checks if char is a digit
+  * @c: int to be checked
+  * Return: 1 if c is digit, 0 otherwise
+  */
+
+int _isdigit(int c)
+{
+	int y = 0;
+
+	if (c >= '0' && c <= '9')
+	{
+		y = 1;
+	}
+	return (y);
+}
