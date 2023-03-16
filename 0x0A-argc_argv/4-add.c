ae54d7c6a7bef73fa8fda2d1198b69838fcfeadc
iAmEmmanuelist <emmanuelpaul152@gmail.com>
2023-03-16 03:59:34 +0100

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,6 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
+	int i, j;
 
 	if (argc == 1)
 	{
@@ -19,9 +20,9 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	for (int i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
